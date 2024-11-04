faeb891e0d8da983e01d0fe42e789532df98c7d1
Mayankk-fexle <mayank.mathur@fexle.com>
2024-11-04 10:49:43 +0530

    added files retrieved from staging.org

--- src/lwc/myComponent/myComponent.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class MyComponent extends LightningElement {
-    greeting = 'World';
-    changeHandler(event) {
-    this.greeting = event.target.value;
-    }
-}
